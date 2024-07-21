350affb2b4492371980ed633b7a75a58fc923b82
Alexsey-Tsipko <popolo090909@yandex.ru>
2024-07-21 10:48:55 +0300

    ПОчти

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import index
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('posts<int:id>/', views.post_detail, name='detail'),
+    path('category/<slug:category_slug>/',  views.category_posts, name='category_posts'),
+]
