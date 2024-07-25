97f08751e18167cc4af1019b44163ffe8ba21b6c
Alina Tsipko <tcipkoalina2000@mail.ru>
2024-07-25 10:57:42 +0300

    25.07.2024

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -8,5 +8,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', index, name='index'),
     path('posts/<int:id>/', views.post_detail, name='detail'),
-    path('category/<slug:category_slug>/',  views.category_posts, name='category'),
+    path('category/<slug:category_slug>/',
+         views.category_posts, name='category'),
 ]
