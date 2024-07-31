09abacbcb1d4ed6944d70c0c3ab240b21d60a833
Alexsey-Tsipko <popolo090909@yandex.ru>
2024-07-31 15:39:20 +0300

    Почти4 3107202445678

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -66,4 +66,3 @@ def category_posts(request, category_slug):
     context = {'category': category_slug,
                'posts': sorted_posts}
     return render(request, 'blog/category.html', context)
-
